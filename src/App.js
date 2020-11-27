38396c474df12ebf19dc7b3a2e838bf326555ba5
Matteo <Rascioni>
2020-11-27 12:54:32 +0100

    Todo List React 100% Completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './App.css'
 
-function App() {
+const Todo = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{textDecoration: props.todo.completato ? 'line-through' : ''}} className="todo">
+      {props.todo.name}
+      <div>
+        <button className="btn-todo" onClick={() => props.completaTodo(props.index)}>
+          Completa
+        </button>
+      </div>
     </div>
+  )
+};
+
+// Form component
+const Form = (props) => {
+    const [value, setValue] = useState()
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      if (value.trim() === '') {
+        alert('scrivi qualcosa da aggiungere alla lista')
+        return 
+      }
+      props.submit(value)
+      setValue('')
+    }
+
+    const onChangeText = (e) => {
+      setValue(e.target.value)
+    }
+
+    return (
+      <form onSubmit={handleSubmit}>
+        <input className="input" type="text" value={value} placeholder="aggiugni todo" onChange={onChangeText}/>
+      </form>
+    )
+};
+
+// App component
+const App = () => { 
+  const [todos, setTodos] = useState(
+    [
+      {name: 'Imparare React', completato: false},
+      {name: 'Imparare gli state', completato: false},
+      {name: 'Imparare i component', completato: true},
+    ]
   );
+     
+  const addTodo = (todo) => {
+    const newTodos = [...todos, {name: todo}]
+    setTodos(newTodos)
+  }
+
+  const completaTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].completato = true;
+    setTodos(newTodos)
+  } 
+
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((item, index) => (
+          <Todo key={index} todo={item} index={index} completaTodo={completaTodo}/>
+        ))}
+        <Form submit={addTodo} />
+      </div>
+    </div>
+  )
 }
 
+
 export default App;
+
